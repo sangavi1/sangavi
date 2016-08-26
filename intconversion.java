7e050b8e4040d1d445b92e518270ea160e1d2833
sangavi1 <sangavisadagoban@gmail.com>
2016-08-27 00:24:30 +0530

    Create intconversion.java

---
+++ intconversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class intconversion
+{
+public static void main(String args[])
+{
+String s;
+Scanner in=new Scanner(System.in);
+s=in.nextLine();
+int n=Integer.parseInt(s);
+System.out.println(n);
+}
+}
