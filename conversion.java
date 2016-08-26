3781b1ca55de8fa4e523fe7c8b8eae39da16bfe0
sangavi1 <sangavisadagoban@gmail.com>
2016-08-27 00:15:28 +0530

    Create conversion.java

---
+++ conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class conversion
+{
+public static void main(String args[])
+{
+String s;
+Scanner in=new Scanner(System.in);
+s=in.nextLine();
+Integer number=Integer.valueOf(s);
+System.out.println(number);
+}
+}
