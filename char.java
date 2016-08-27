8ee89cdc812d62a0a3c483007aab0124ad73f1e5
sangavi1 <sangavisadagoban@gmail.com>
2016-08-27 20:39:35 +0530

    Create char.java

---
+++ char.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class char 
+{
+public  static void main(String args[])
+{
+char a;
+Scanner in=new Scanner(System.in)
+a=in.next();
+if(a>="a"&&a<="z")
+{
+System.out.println("alphapet");
+}
+else
+{
+System.out.println("not alphapet");
+}
+}
+}
