d553acfb84c9780061e5bba803b8283b33dde2bd
sangavi1 <sangavisadagoban@gmail.com>
2016-08-28 18:37:18 +0530

    Create two.java

---
+++ two.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class two
+{
+public static void main(String args[])
+{
+int n,count=0;
+Scanner in=new Scanner(System.in);
+n=n.nextInt();
+for(i=0;i<=n;i++)
+{
+if(i==2)
+{
+count++;
+}
+}
+System.out.println(count);
+}
+}
+
