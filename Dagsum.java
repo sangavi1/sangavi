6a4ac67ca2bc331e71bcabf0323d5ae75f0b8988
sangavi1 <sangavisadagoban@gmail.com>
2016-09-10 18:14:56 +0530

    Create Dagsum.java

---
+++ Dagsum.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Dagsum{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scr=new Scanner(System.in);
+		System.out.println("Enter the Dag");
+		int sum=0;
+		String str=scr.nextLine();
+		char c=(char)(92);
+		str=str.replace(c, ',');
+		
+	String[] b=str.split(','+"");
+	for(int i=0;i<b.length;i++){
+		int max=0;
+		String[] abc=b[i].split(" ");
+		for(int j=0;j<abc.length;j++){
+			
+			if(!(abc[j].equals("/")||abc[j].equals("")))
+				{
+				int v=Integer.parseInt(abc[j]);
+				if(v>max)
+					max=v;
+				}
+		}
+		sum+=max;
+	}
+System.out.println(sum);
+	}
+
+}
