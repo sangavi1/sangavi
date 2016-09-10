6b0daf3d32ced4776fcf3c6959b5a26a933bc4ac
sangavi1 <sangavisadagoban@gmail.com>
2016-09-10 18:54:22 +0530

    Create Hunter3 .java

---
+++ Hunter3 .java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+
+
+public class Hunter3 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> n=new ArrayList<Integer>();
+		int i=0,m=0,t=0,sum=0;
+		Scanner s=new Scanner(System.in);
+		boolean b=true;
+		System.out.println("how many elements do u wanna insert");
+		int a=s.nextInt();
+		for(i=0;i<a;i++)
+		{
+			n.add(s.nextInt());
+			//System.out.println(n.get(i));
+		}
+		System.out.println("enter target number t");
+		t=s.nextInt();
+		System.out.println("enter m elements to be added");
+		m=s.nextInt();
+		for(int j=0;j<m;j++)
+		{
+			System.out.println(n.get(j));
+			sum=sum+n.get(j);
+		}
+		System.out.println(sum);
+		if(sum==t)
+		{
+			System.out.println(b);
+			
+		}
+		else
+		{
+			System.out.println("false");
+		}
+		
+		s.close();
+	}
+
+}
