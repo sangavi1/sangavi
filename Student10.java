e68708437d2f1ec3c861d2b2303dd4bddabc6780
sangavi1 <sangavisadagoban@gmail.com>
2016-09-17 14:11:10 +0530

    Create Student10.java

---
+++ Student10.java
@@ -0,0 +1,17 @@
+class Student10{  
+    int id;  
+    String name;  
+      
+    Student10(int id,String name){  
+    id = id;  
+    name = name;  
+    }  
+    void display(){System.out.println(id+" "+name);}  
+  
+    public static void main(String args[]){  
+    Student10 s1 = new Student10(111,"Karan");  
+    Student10 s2 = new Student10(321,"Aryan");  
+    s1.display();  
+    s2.display();  
+    }  
+}  
